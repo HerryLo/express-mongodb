4fbb994c3ab368ccb2a87f97f55b501f76566647
HerryLo <1169170165@qq.com>
2017-11-19 20:20:11 +0800

    保存mock数据

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  next();
+});
+
 app.use('/', index);
-movie
 app.use('/users', users);
 app.use('/api/movie', movie);
 
--- routes/movie.js
+++ routes/movie.js
@@ -6,25 +6,25 @@ var Random = Mock.Random
 let color = Random.color()
 let word = Random.word(5)
 
-app.get('/api/movie', (req, res) => {
+router.get('/', (req, res) => {
     let data = Mock.mock({
-      '影院热映|7': [{
+      'm1|7': [{
         'id|+1': 1,
-        'img1': `@IMAGE(271x381, ${color}, #FFF, ${word})`,
+        'img': `@IMAGE(271x381, ${color}, #FFF, ${word})`,
         'name': '@Name',
         'title': '@TiTLE',
         'type': 'movie'
       }],
-      '免费在线观影|7': [{
+      'm2|7': [{
         'id|+1': 10,
-        'img1': `@IMAGE(271x381, ${color}, #FFF, ${word})`,
+        'img': `@IMAGE(271x381, ${color}, #FFF, ${word})`,
         'name': '@Name',
         'title': '@TiTLE',
         'type': 'movie'
       }],
-      '新片速递|7': [{
+      'm3|7': [{
         'id|+1': 20,
-        'img1': `@IMAGE(271x381, ${color}, #FFF, ${word})`,
+        'img': `@IMAGE(271x381, ${color}, #FFF, ${word})`,
         'name': '@Name',
         'title': '@TiTLE',
         'type': 'movie'
